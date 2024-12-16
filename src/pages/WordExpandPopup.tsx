1fc2650267df8548628acda9b63b9258197086b7
RishabhKumar2901 <rishaabkumar09@gmail.com>
2024-12-16 16:47:30 +0530

    add features in iframe popup

--- src/pages/WordExpandPopup.tsx
+++ src/pages/WordExpandPopup.tsx
@@ -10,18 +10,24 @@ const Popup: React.FC<PopupProps> = ({ word }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 w-full">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
         <h2 className="text-xl font-bold">{word?.word}</h2>
-        <p className="mt-2"><strong>Meaning:</strong> {word?.meaning}</p>
+        <p className="mt-2">
+          <strong>Meaning:</strong> {word?.meaning}
+        </p>
         {word?.purchasedby && (
           <div className="mt-2">
             <strong>Assigned to:</strong> {word?.purchasedby}
           </div>
         )}
         {word?.url && (
-          <iframe
-            className="mt-4 h-80 w-full"
-            src={word?.url}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          ></iframe>
+          <>
+            <iframe
+              className="mt-4 h-80 w-full"
+              src={word?.url}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          </>
         )}
       </div>
     </div>
