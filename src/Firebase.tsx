73b9e625b5b09212461b0a1b66a8fd704d5a4741
RishabhKumar2901 <rishaabkumar09@gmail.com>
2024-12-16 13:09:21 +0530

    integrate firebase

---
+++ src/Firebase.tsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOlRjW-Gu_WiQrgvOecn-xvRTccNM6Ny4",
+  authDomain: "eqwebsite-cfebb.firebaseapp.com",
+  projectId: "eqwebsite-cfebb",
+  storageBucket: "eqwebsite-cfebb.firebasestorage.app",
+  messagingSenderId: "686751750117",
+  appId: "1:686751750117:web:d79785a749e2d58d10410c"
+};
+
+export const app = initializeApp(firebaseConfig);
