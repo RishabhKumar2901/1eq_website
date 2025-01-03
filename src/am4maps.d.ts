e156bff514244628ed28d0ffb57fdfdcfa90a446
Rishabhk1234 <134509951+Rishabhk1234@users.noreply.github.com>
2025-01-03 14:31:31 +0530

    show states and counts in heatmap

---
+++ src/am4maps.d.ts
@@ -0,0 +1,10 @@
+declare global {
+    interface Window {
+      am4core: any;
+      am4maps: any;
+      am4geodata_india2019High: any;
+      am4themes_animated: any;
+    }
+  }
+  
+  export {};  
--- src/pages/HeatMap.tsx
+++ src/pages/HeatMap.tsx
@@ -1,93 +1,151 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Papa from "papaparse";
-import { Chart } from "react-google-charts";
+import "tailwindcss/tailwind.css";
 import { PincodeData } from "../PincodeData";
 import { getUserCountByPincode } from "../static/Functions";
+import { stateToIdMapping } from "../static/Variables";
 
-type PinCodeCount = Record<string, number>;
+const HeatMap: React.FC = () => {
+  const [pinCodeCount, setPinCodeCount] = useState<Record<string, number>>({});
+  const [statePinCodeCount, setStatePinCodeCount] = useState<
+    Record<string, number>
+  >({});
 
-const HeatMap = () => {
-    const [pinCodeCount, setPinCodeCount] = useState<PinCodeCount>({});
-    const [statePinCodeCount, setStatePinCodeCount] = useState<PinCodeCount>({});
-    const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    getUserCountByPincode().then((data) => {
+      setPinCodeCount(data);
+    });
+  }, []);
 
-    useEffect(() => {
-        const parseCSV = () => {
-            Papa.parse(PincodeData, {
-                complete: (result: any) => {
-                    const stateCount: PinCodeCount = { ...statePinCodeCount };
-                    const headers = result.meta.fields || [];
-                    const stateIndex = headers.indexOf("State");
-                    const pincodeIndex = headers.indexOf("Pincode");
+  useEffect(() => {
+    const parseCSV = () => {
+      Papa.parse(PincodeData, {
+        header: true,
+        skipEmptyLines: true,
+        complete: (result) => {
+          const stateCounts: Record<string, number> = {};
+          result.data.forEach((row: any) => {
+            const stateName = row.State?.trim().toUpperCase();
+            const pincode = row.Pincode?.trim();
+            const id = stateToIdMapping[stateName];
 
-                    if (stateIndex === -1 || pincodeIndex === -1) {
-                        console.error("Missing 'statename' or 'pincode' in CSV.");
-                        return;
-                    }
+            if (id && pincode && pinCodeCount[pincode]) {
+              stateCounts[id] = (stateCounts[id] || 0) + pinCodeCount[pincode];
+              delete pinCodeCount[pincode];
+            }
+          });
+          setStatePinCodeCount(stateCounts);
+        },
+        error: (error: any) => {
+          console.error("Error parsing CSV:", error);
+        },
+      });
+    };
 
-                    result.data.forEach((row: any) => {
-                        const state = row[headers[stateIndex]]?.trim().toUpperCase();
-                        const pincode = row[headers[pincodeIndex]];
+    if (Object.keys(pinCodeCount).length > 0) {
+      parseCSV();
+    }
+  }, [pinCodeCount]);
 
-                        if (state && pincode && pinCodeCount[pincode]) {
-                            if (stateCount[state]) {
-                                stateCount[state] = stateCount[state] + pinCodeCount[pincode];
-                            } else {
-                                stateCount[state] = pinCodeCount[pincode];
-                            }
-                            delete pinCodeCount[pincode];
-                        }
-                    });
-                    setStatePinCodeCount(stateCount);
-                    setLoading(false);
-                },
-                header: true,
-                skipEmptyLines: true,
-            });
-        };
+  useEffect(() => {
+    if (window.am4core && window.am4maps && statePinCodeCount) {
+      const am4core = window.am4core;
+      const am4maps = window.am4maps;
+      const am4geodata_india2019High = window.am4geodata_india2019High;
+      const am4themes_animated = window.am4themes_animated;
 
-        if (PincodeData) {
-            parseCSV();
+      am4core.useTheme(am4themes_animated);
+
+      const chart = am4core.create("chartdiv", am4maps.MapChart);
+      chart.geodata = am4geodata_india2019High;
+
+      const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+      polygonSeries.useGeodata = true;
+
+      polygonSeries.data = Object.entries(statePinCodeCount).map(
+        ([id, count]) => ({
+          id,
+          value: count,
+        })
+      );
+
+      polygonSeries.heatRules.push({
+        property: "fill",
+        target: polygonSeries.mapPolygons.template,
+        min: chart.colors.getIndex(1).brighten(1),
+        max: chart.colors.getIndex(1).brighten(-0.3),
+      });
+
+      const polygonTemplate = polygonSeries.mapPolygons.template;
+      polygonTemplate.events.on("over", function (event: any) {
+        const polygon = event.target;
+        const value = polygon.dataItem?.dataContext?.value;
+        if (value > 0) {
+          polygon.tooltipText = `${polygon.dataItem.dataContext.name}: ${value}`;
+        } else {
+          // polygon.tooltipText = polygon.dataItem.dataContext.name;
+          polygon.tooltipText = `${polygon.dataItem.dataContext.name}: 0`;
         }
-    }, [pinCodeCount]);
+      });
+      polygonTemplate.nonScalingStroke = true;
+      polygonTemplate.strokeWidth = 0.5;
 
-    useEffect(() => {
-        getUserCountByPincode().then(setPinCodeCount);
-    }, []);
+      const hs = polygonTemplate.states.create("hover");
+      hs.properties.fill = am4core.color("#3c5bdc");
 
-    const geoData = [
-        ["State", "Users"],
-        ...Object.entries(statePinCodeCount).map(([state, count]) => [
-            state,
-            count,
-        ]),
-    ];
+      const imageSeries = chart.series.push(new am4maps.MapImageSeries());
+      imageSeries.mapImages.template.nonScaling = true;
 
-    return (
-        <>
-            {loading ? (
-                <div className="flex w-full flex-wrap justify-center items-center h-96">
-                    <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-                </div>
-            ) : (
-                <div className="relative w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
-                    <Chart
-                        chartType="GeoChart"
-                        data={geoData}
-                        options={{
-                            region: "IN",
-                            displayMode: "regions",
-                            resolution: "provinces",
-                            colorAxis: {
-                                colors: ["#ffcccc", "#ff3300"],
-                            },
-                            tooltip: { trigger: 'focus' },
-                        }}
-                    />
-                </div>
-            )}
-        </>
-    );
+      const label = imageSeries.mapImages.template.createChild(am4core.Label);
+      label.text = `{name}\n{value}`;
+      label.horizontalCenter = "middle";
+      label.verticalCenter = "middle";
+      label.textAlign = "middle";
+      label.fontSize = 14;
+      // label.fill = am4core.color("#FFFFFF");
+      label.nonScaling = true;
+
+      imageSeries.data = polygonSeries.data.map((item: any) => {
+        const stateFeature = chart.geodata.features.find(
+          (feature: any) => feature.id === item.id
+        );
+
+        if (stateFeature) {
+          const centroid = stateFeature.geometry.coordinates[0];
+          const center = centroid.reduce(
+            (acc: { lat: number; lon: number }, coord: [number, number]) => {
+              acc.lat += coord[1];
+              acc.lon += coord[0];
+              return acc;
+            },
+            { lat: 0, lon: 0 }
+          );
+
+          const numPoints = centroid.length;
+          return {
+            id: item.id,
+            name: chart.geodata.features.find(
+              (feature: any) => feature.id === item.id
+            )?.properties.name,
+            latitude: center.lat / numPoints,
+            longitude: center.lon / numPoints,
+            value: item.value,
+          };
+        }
+
+        return {};
+      });
+
+      imageSeries.mapImages.template.propertyFields.latitude = "latitude";
+      imageSeries.mapImages.template.propertyFields.longitude = "longitude";
+
+      return () => {
+        chart.dispose();
+      };
+    }
+  }, [statePinCodeCount]);
+
+  return <div id="chartdiv" className="w-full h-screen bg-gray-100"></div>;
 };
 
 export default HeatMap;
